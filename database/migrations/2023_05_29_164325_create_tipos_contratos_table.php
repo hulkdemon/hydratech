b54ea9cd0f77cb8f7c2ef5b617cd28649230c09e
Gil-GBR <gilberto.bramasco@ibanor.mx>
2023-06-01 14:26:51 -0700

    se agrego el campo timestamps en la tabla tipos_contratos

--- database/migrations/2023_05_29_164325_create_tipos_contratos_table.php
+++ database/migrations/2023_05_29_164325_create_tipos_contratos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tipos_contratos', function (Blueprint $table) {
             $table->id('id_tipo_contrato');
             $table->string('nombre');
+            $table->timestamps();
         });
     }
 
