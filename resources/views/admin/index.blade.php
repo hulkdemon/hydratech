304fcfb70ffdf94ae80363e83e1f23f943c7f774
Anthony <antonhy.lamas@ibanor.mx>
2023-06-14 10:39:58 -0700

    Agregacion de AdminLte ala branch con el login y migraciones

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'HydraTech')
+
+@section('content_header')
+    <h1>HydraTech</h1>
+@stop
+
+@section('content')
+    <p>Pagina Principal HydraTech.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
