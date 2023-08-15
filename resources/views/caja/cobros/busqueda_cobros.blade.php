b82eca6cd9633272464e1b41fda42bc2e2b6e2e6
Antonio Andrade <hulkdemon321@gmail.com>
2023-08-14 18:03:41 -0700

    modificacion de prueba para agregar los botones de pdf, excel y etc a la tabla de busqueda de contratos

--- resources/views/caja/cobros/busqueda_cobros.blade.php
+++ resources/views/caja/cobros/busqueda_cobros.blade.php
@@ -4,7 +4,9 @@
 @section('title', 'Búsqueda Contratos')
 
 @section('content')
+
     <body> 
+       
         <script src="https://kit.fontawesome.com/42813926db.js" crossorigin="anonymous"></script>
         <section class="content-header">
             <div class="container-fluid">
@@ -28,13 +30,20 @@
         <div class="card">
             <div class="card-header">
                 <div class="d-flex justify-content-between align-items-center">
-                    <h4 class="mb-0">Contratos Registrados</h4>
-                    <a href="{{ url('caja/contratos/registrar_contrato') }}" class="btn btn-primary">Registrar nuevo contrato</a>
+                    <h4 class="mb-0">Contratos registrados</h4>
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa-solid fa-tools"></i> Opciones de Gestión
+                      </button>
+                      <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="{{url ("admin")}}"><i class="fa-solid fa-file-signature"></i> Registrar contrato</a></li>
+                        <li class="dropdown-item"><a href="#"><i class="fa-solid fa-folder-open"></i> Registrar tipo de contrato</a></li>
+                      </ul>                
                 </div>
             </div>
             <div class="card-body">
                 <h5 class="mb-4">Realizar búsqueda por nombre, apellido, domicilio o número de contrato</h5>
                 <table id="datos" class="table table-bordered table-striped">
+                    <h5 class="m-0">Funciones de la tabla</h5>
                     <thead>
                         <tr>
                             <th>#</th>
@@ -54,7 +63,7 @@
                             <td>
                                 <div class="d-flex justify-content-between">
                                     <button class="btn btn-primary btn-sm"><i class="fa-solid fa-credit-card"></i>  Realizar cobro</button>
-                                    <button class="btn btn-success btn-sm"><i class="fa-solid fa-wallet"></i> Registrar abono </button>
+                                    <button class="btn btn-success btn-sm"><i class="fa-solid fa-wallet"></i> Registrar abono</button>
                                     <button class="btn btn-info btn-sm"><i class="fa-solid fa-hand-holding-dollar"></i> Asignar condonación</button>
                                 </div>
                             </td>
@@ -70,47 +79,74 @@
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-    
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<!-- DataTables Buttons CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+
 @stop
 
 @section('js')
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  <!-- DataTables -->
+  <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+  <!-- DataTables Buttons -->
+  <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+  <!-- JSZip -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+  <!-- PDFMake -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <!-- DataTables Buttons HTML5 -->
+  <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+
 <script>
+    
     $(function () {
-        $("#datos").DataTable({
-            "responsive": true,
-            "lengthChange": true,
-            "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
-            "language": {
-                "sProcessing": "Procesando...",
-                "sLengthMenu": "Mostrar _MENU_ registros",
-                "sZeroRecords": "No se encontraron resultados",
-                "sEmptyTable": "Ningún dato disponible en esta tabla",
-                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix": "",
-                "sSearch": "Buscar:",
-                "sUrl": "",
-                "sInfoThousands": ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "sNext": "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                },
-                "buttons": {
-                    "copy": "Copiar",
-                    "colvis": "Visibilidad"
-                }
+    $("#datos").DataTable({
+        "responsive": true,
+        "lengthChange": true,
+        "autoWidth": true,
+        dom: 'Bfrtip',
+        buttons: [
+            'excelHtml5',
+            'csvHtml5',
+            'print',
+            'pdfHtml5',
+            'colvis'
+        ],
+        "language": {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "print": "Imprimir",
+                "colvis": "Visibilidad"
             }
-        });
+        }
     });
+});
     </script>
 @stop
