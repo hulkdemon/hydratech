cd80df84b5b7cfcf272d92b6626efdbffee73709
hulkdemon <134327024+hulkdemon@users.noreply.github.com>
2023-08-17 15:38:07 -0700

    Update 2023_05_29_165327_create_cobros_conceptos_table.php

--- database/migrations/2023_05_29_165327_create_cobros_conceptos_table.php
+++ database/migrations/2023_05_29_165327_create_cobros_conceptos_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('cobros_conceptos', function (Blueprint $table) {
             $table->id('id_cobro_concepto');
-            $table->unsignedBigInteger('id_cobro');
+            $table->unsignedBigInteger('id_contrato');
             $table->unsignedBigInteger('id_concepto');
-            $table->foreign('id_cobro')->references('id_cobro')->on('cobros');
+            $table->foreign('id_contrato')->references('id_contrato')->on('contratos');
             $table->foreign('id_concepto')->references('id_concepto')->on('conceptos');
             $table->timestamps();
         });
